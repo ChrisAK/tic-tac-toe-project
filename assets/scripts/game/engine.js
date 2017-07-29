fb482c7d1b79c85896885bed5916ca40a0ddb141
ChrisAK <ckennelly8910@gmail.com>
2017-07-29 11:43:00 -0400

    Add board constructor
    -Built a constructor to make the game board. Not sure if I should have
    filled the newBoard array with null (which I did) or leave it empty.
    Reading up on null made it seem like that was the way to go, seeing as I
    want the array to have 9 indexes (0-8) with no value.

---
+++ assets/scripts/game/engine.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// TODO Empty array or filled with null/undefined?
+
+// needs a way to check for wins and ties
+//
+
+// constructor to make a game board! this.newBoard will be used for reset
+// this.board will be used for actual play.
+// this.winState is all of the possible ways to win
+// this.gameEnd says whether the game is over or not
+// project said to assume x went first (this.currentPlayer = 'x')
+
+const TicTacToe = function () {
+  this.newBoard = [null, null, null, null, null, null, null, null, null]
+  this.board = this.newBoard.slice()
+  this.winState = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
+  this.gameEnd = false
+  this.currenPlayer = 'x'
+}
